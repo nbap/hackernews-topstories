3c99ea9c8a3e792de0f43ebfe50e92662fa55be9
Nicolas Baptista <nicolas@nbap.com.br>
2020-06-16 21:47:21 -0300

    Modify fetcher to retrieve topstories from Hnews

--- lib/hn_aggregator/hnews/fetcher.ex
+++ lib/hn_aggregator/hnews/fetcher.ex
@@ -3,6 +3,12 @@ defmodule HnAggregator.Hnews.Fetcher do
 
   use Task
 
+  alias HnAggregator.Hnews.Api
+
+  # To prevent hammering hnews's api, apply a little sleep between requests
+  @sleep_between_requests 500
+  @yield_tasks_interval 4 * 60 * 1000
+  # Application.get_env(:hn_aggregator, :news_fetcher_interval)
   @fetcher_interval 15_000
 
   def start_link(opts) do
@@ -23,6 +29,58 @@ defmodule HnAggregator.Hnews.Fetcher do
   end
 
   def fetch_topstories(fetch_max \\ 5) when fetch_max > 0 do
-    Logger.info("top stories retrieved")
+    with {:ok, %{body: [_ | _], status_code: 200} = response} <- Api.topstories() do
+      topstories =
+        response.body
+        |> Enum.slice(0..(fetch_max - 1))
+        |> fetch_stories()
+
+      Logger.info("#{length(topstories)} top stories retrieved")
+
+      :ok
+    else
+      {:error, response} ->
+        Logger.error("An error occurred when retrieving topstories \n #{inspect(response)}")
+        :error
+
+      _ ->
+        :error
+    end
+  end
+
+  def fetch_story(story_id) when is_integer(story_id) do
+    fetch_story(Integer.to_string(story_id))
+  end
+
+  def fetch_story(story_id) when is_binary(story_id) do
+    Task.async(fn ->
+      Process.sleep(@sleep_between_requests)
+      Api.item(story_id)
+    end)
+  end
+
+  def fetch_stories(stories_ids) when is_list(stories_ids) do
+    stories_ids
+    |> Enum.map(&fetch_story/1)
+    |> Task.yield_many(@yield_tasks_interval)
+    |> Enum.map(fn {task, result} ->
+      result || Task.shutdown(task, :brutal_kill)
+    end)
+    |> Enum.map(fn
+      {:ok, value} ->
+        with {:ok, %{body: %{}, status_code: 200} = response} <- value do
+          response.body
+        else
+          error ->
+            Logger.error("An error occurred when retrieving an story \n #{inspect(error)}")
+            :error
+        end
+
+      error ->
+        Logger.error("An error occurred when retrieving an story \n #{inspect(error)}")
+        :error
+    end)
+    |> Enum.filter(fn story -> story != :error end)
+    |> Enum.sort_by(& &1["score"], :desc)
   end
 end
