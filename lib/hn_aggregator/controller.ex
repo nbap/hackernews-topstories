f13e774168c3dbdd475cca4d084b9df435ecc103
Nicolas Baptista <nicolas@nbap.com.br>
2020-06-17 20:08:51 -0300

    Replace mocked responses with topstories

--- lib/hn_aggregator/controller.ex
+++ lib/hn_aggregator/controller.ex
@@ -1,20 +1,28 @@
 defmodule HnAggregator.Controller do
   use Plug.Router
 
+  alias HnAggregator.Hnews
+
   plug(:match)
   plug(Plug.Parsers, parsers: [:json], json_decoder: Jason)
   plug(:dispatch)
 
   get "/stories" do
+    %{stories: stories, inserted_at: inserted_at} = Hnews.Service.get_topstories()
+
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, "hello")
+    |> put_resp_header("last-modified", DateTime.to_string(inserted_at))
+    |> send_resp(200, Jason.encode!(stories))
   end
 
   get "/stories/:story_id" do
+    %{story: story, inserted_at: inserted_at} = Hnews.Service.get_story(story_id)
+
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, "hello")
+    |> put_resp_header("last-modified", DateTime.to_string(inserted_at))
+    |> send_resp(200, Jason.encode!(story))
   end
 
   match _ do
